7e0181b5321cff40eb5bbea41bb1a08adbb62aeb
Joshua Wise <jwise@andrew.cmu.edu>
2010-12-20 04:26:16 -0500

    Add stuff to make us a service.

--- src/com/unrevoked/Zysploit/Zysploit.java
+++ src/com/unrevoked/Zysploit/Zysploit.java
@@ -1,15 +1,45 @@
 package com.unrevoked.Zysploit;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.lang.*;
+import android.app.Service;
+import android.widget.Toast;
+import android.util.Log;
+import android.os.Looper;
+import android.os.IBinder;
+import android.content.Intent;
 
-public class Zysploit extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class Zysploit extends Service implements Runnable {
+	private Thread me;
+	
+	private void say(String s) {
+		Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Override
+	public void run()
+	{
+		Looper.prepare();
+		
+		Log.v("Zysploit", "Inside thread.");
+		
+		say("Inside thread.");
+		
+	}
+	
+	@Override
+	public void onStart(Intent i, int startId) {
+		Log.v("Zysploit", "Started.");
+		
+		say("Outisde thread.");
+		
+		me = new Thread(this, "Zysploit thread");
+		me.start();
+	}
+	
+	@Override
+	public IBinder onBind(Intent i) {
+		Log.v("Zysploit", "Bound.");
+		
+		return null;
+	}
 }
